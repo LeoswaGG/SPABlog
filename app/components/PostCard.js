1b49644ca3f1f080ac7c10e7bda566aa16c38aee
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-24 11:12:32 -0600

    Refactoriza el componente para que acepte un objeto como parámetro que contiene los datos de cada post

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,16 +1,11 @@
 /**
- * The `PostCard` function generates a section element with multiple post cards, each containing a
- * title, image, and date.
- * @param titles - An array of post titles.
- * @param images - The `images` parameter is an array that contains the names of the image files. These
- * image files are located in the "app/assets/" directory. The images are used to display a post image
- * for each post in the PostCard component.
- * @param date - The `date` parameter is an array that contains the dates of the posts. Each element in
- * the array represents the date of a specific post.
- * @returns a dynamically created HTML section element that contains multiple post cards. Each post
- * card includes a title, image, and date.
+ * The function `PostCard` creates a section element with multiple post cards based on the data
+ * provided(date, title, img).
+ * @param postsData - The `postsData` parameter is an object that contains the data for each post. It
+ * has the following structure:
+ * @returns a dynamically created HTML section element that contains multiple post cards.
  */
-export function PostCard ( titles, images, date ) {
+export function PostCard ( postsData ) {
     const $section = document.createElement( 'section' );
     $section.innerHTML = `
     <div class="post-container">
@@ -18,11 +13,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 0 ] }</span>
+                    <span>${ postsData.post1.date }</span>
                 </div>
-                <p class="post-title">${ titles[ 0 ] }</p>
+                <p class="post-title">${ postsData.post1.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 0 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post1.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
@@ -33,11 +28,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 1 ] } </span>
+                    <span>${ postsData.post2.date } </span>
                 </div>
-                <p class="post-title">${ titles[ 1 ] }</p>
+                <p class="post-title">${ postsData.post2.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 1 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post2.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
@@ -48,11 +43,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 2 ] } </span>
+                    <span>${ postsData.post3.date } </span>
                 </div>
-                <p class="post-title">${ titles[ 2 ] }</p>
+                <p class="post-title">${ postsData.post3.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 2 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post3.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
@@ -63,11 +58,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 3 ] } </span>
+                    <span>${ postsData.post4.date } </span>
                 </div>
-                <p class="post-title">${ titles[ 3 ] }</p>
+                <p class="post-title">${ postsData.post4.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 3 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post4.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
@@ -78,11 +73,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 4 ] } </span>
+                    <span>${ postsData.post5.date } </span>
                 </div>
-                <p class="post-title">${ titles[ 4 ] }</p>
+                <p class="post-title">${ postsData.post5.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 4 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post5.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
@@ -93,11 +88,11 @@ export function PostCard ( titles, images, date ) {
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ date[ 5 ] } </span>
+                    <span>${ postsData.post6.date } </span>
                 </div>
-                <p class="post-title">${ titles[ 5 ] }</p>
+                <p class="post-title">${ postsData.post6.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ images[ 5 ] }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.post6.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
