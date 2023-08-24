2bbb40cd1d8465ca500a8bfaf107a1ddbd51354b
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-24 11:09:39 -0600

    crea el archivo quotes para ahi almacenar y gestionar el contenido de las frases y no mezclarlo con el otro código

---
+++ app/helpers/quotesData.js
@@ -0,0 +1,29 @@
+/* The code is exporting an object called `quotes` which contains six quotes. Each quote is represented
+as a key-value pair within the `quotes` object. The key represents the quote number (`quote1`,
+`quote2`, etc.), and the value is an object containing the quote itself and the author. */
+export const quotes = {
+    quote1: {
+        quote: 'El éxito son las metas, todo lo demás son comentarios',
+        author: 'Brian Tracy'
+    },
+    quote2: {
+        quote: 'Somos lo que somos porque primero lo hemos imaginado',
+        author: 'Donald Curtis'
+    },
+    quote3: {
+        quote: 'Cada ser nació con algún destino, ¿para qué fuiste creado tú?',
+        author: 'Marco Aurelio'
+    },
+    quote4: {
+        quote: 'Si no puedes controlar tu mente, entonces no puedes controlar nada más',
+        author: 'Napoleon Hill'
+    },
+    quote5: {
+        quote: 'La negación es la máxima zona de confort',
+        author: 'David Goggins'
+    },
+    quote6: {
+        quote: 'Un hombre es dueño de su destino en la medida en que es dueño de su atención',
+        author: 'La ducha fría'
+    }
+};
