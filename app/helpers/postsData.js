494c84049d1d9df26247c35957d0b71a1f532a46
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-24 11:08:18 -0600

    crea el archivo postsData para almacenar y gestionar el contenido de los post y no mezclarlo con el demás código

---
+++ app/helpers/postsData.js
@@ -0,0 +1,134 @@
+/* The code is defining an object called `postsData` which contains four properties: `disciplinePosts`,
+`productivityPosts`, `mindSetPosts`, and `masculinityPosts`. Each of these properties is an object
+itself, containing six posts. Each post has three properties: `date`, `title`, and `img`. This code is likely 
+used to store and organize data for a blog or website that features posts related to different topics. */
+export const postsData = {
+    disciplinePosts: {
+        post1: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        },
+        post2: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        },
+        post3: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        },
+        post4: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        },
+        post5: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        },
+        post6: {
+            date: 'disciplina',
+            title: 'es',
+            img: 'todo'
+        }
+    },
+    productivityPosts: {
+        post1: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        },
+        post2: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        },
+        post3: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        },
+        post4: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        },
+        post5: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        },
+        post6: {
+            date: 'productividad',
+            title: 'es',
+            img: 'todo'
+        }
+    },
+    mindSetPosts: {
+        post1: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post2: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post3: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post4: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post5: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post6: {
+            date: 'mentalidad',
+            title: 'es',
+            img: 'todo'
+        }
+    },
+    masculinityPosts: {
+        post1: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post2: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post3: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post4: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post5: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        },
+        post6: {
+            date: 'masculinidad',
+            title: 'es',
+            img: 'todo'
+        }
+    }
+};
