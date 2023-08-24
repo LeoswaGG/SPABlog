1212dbedc1d9cbe9ee27a38560b8550333a7cc7a
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-23 18:01:04 -0600

    Carga dinámicamente el componente 'OurPact' en la ruta del home y Carga dinámicamente las frases del home pasandole al componte 'Phrases' un objeto como parámetro que contiene 6 objetos llamado quote enumerados del 1 al 6 con 2 propiedades: 'quote' y 'author'

--- app/components/Router.js
+++ app/components/Router.js
@@ -10,6 +10,7 @@ import { Hero } from "./Hero.js";
 import { About } from "./About.js";
 import { HeaderSection } from "./HeaderSection.js";
 import { PostCard } from "./PostCard.js";
+import { OurPact } from "./OurPactSection.js";
 
 export function Router () { // aquí no puedes usar await porque el código no devuelve una promesa
     const $main = document.getElementById( 'main' );
@@ -23,7 +24,33 @@ export function Router () { // aquí no puedes usar await porque el código no d
             $main.appendChild( Hero() );
             $main.appendChild( About() );
             $main.appendChild( PostHome() );
-            $main.appendChild( Phrases() );
+            $main.appendChild( Phrases( {
+                quote1: {
+                    quote: 'El éxito son las metas, todo lo demás son comentarios',
+                    author: 'Brian Tracy'
+                },
+                quote2: {
+                    quote: 'Somos lo que somos porque primero lo hemos imaginado',
+                    author: 'Donald Curtis'
+                },
+                quote3: {
+                    quote: 'Cada ser nació con algún destino, ¿para qué fuiste creado tú?',
+                    author: 'Marco Aurelio'
+                },
+                quote4: {
+                    quote: 'Si no puedes controlar tu mente, entonces no puedes controlar nada más',
+                    author: 'Napoleon Hill'
+                },
+                quote5: {
+                    quote: 'La negación es la máxima zona de confort',
+                    author: 'David Goggins'
+                },
+                quote6: {
+                    quote: 'Un hombre es dueño de su destino en la medida en que es dueño de su atención',
+                    author: 'La ducha fría'
+                }
+            } ) );
+            $main.appendChild( OurPact() );
             $main.appendChild( ContactForm() );
             $loader.style.display = 'none';
         }, 500 );
