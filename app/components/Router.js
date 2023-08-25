839a2ab7bd694dd3d6991ad4457087748e361e12
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-25 11:55:49 -0600

    Refactoriza el código, cualquier fallo que presente es a partir de este cambio. Importa el componente que contiene la estructura del contenido de cada artículo. Recupera de la url la categoria y el id de cada artículo. Inserta un ciclo for-in en cada ruta para cargar una tarjeta del post cada que agregue un nuevo post en el archivo postsData.js. Agrega una nueva condición que verifica que el hash contenga 'artículo/' si lo tiene recupera el id y la categoria de la url e insertale al main el componente postContainer para que cargue solo la información de ese artículo con la categoria y el id especificados. Si no contiene el id o la categoria en la url muestra un error.

--- app/components/Router.js
+++ app/components/Router.js
@@ -1,7 +1,7 @@
 /**
  * The `Router` function is responsible for updating the content of the `main` element based on the
  * current URL hash, and it also handles error cases.
- */
+*/
 import { Error404 } from "./Error404.js";
 import { Phrases } from "./Phrases.js";
 import { PostHome } from "./PostHome.js";
@@ -13,11 +13,14 @@ import { PostCard } from "./PostCard.js";
 import { OurPact } from "./OurPactSection.js";
 import { postsData } from "../helpers/postsData.js";
 import { quotes } from "../helpers/quotesData.js";
+import { PostContainer } from "./PostContainer.js";
 
 export function Router () { // aquí no puedes usar await porque el código no devuelve una promesa
     const $main = document.getElementById( 'main' );
     const $root = document.getElementById( 'root' );
     const $loader = document.querySelector( '.loader' );
+    const categoryPostUrl = location.hash.split( '/' )[ 1 ];
+    const idPostUrl = location.hash.split( '/' )[ 3 ];
     const { hash } = location;
     $main.innerHTML = null; // cleans the page before charges content 
     document.querySelector( '.loader' ).style.display = 'block';
@@ -34,25 +37,72 @@ export function Router () { // aquí no puedes usar await porque el código no d
     } else if ( hash === '#/disciplina' ) {
         setTimeout( () => {
             $main.appendChild( HeaderSection( 'Dominio Propio' ) );
-            $main.appendChild( PostCard( postsData.disciplinePosts ) );
+            const $fragment = document.createDocumentFragment();
+            const $section = document.createElement( 'section' );
+            const $divContainer = document.createElement( 'div' );
+            $divContainer.classList.add( 'post-container' );
+            for ( const post in postsData.disciplina ) {
+                $fragment.appendChild( PostCard( postsData.disciplina[ post ] ) );
+            }
+            $divContainer.appendChild( $fragment );
+            $section.appendChild( $divContainer );
+            $main.appendChild( $section );
             $loader.style.display = 'none';
         }, 500 );
     } else if ( hash === '#/productividad' ) {
         setTimeout( () => {
             $main.appendChild( HeaderSection( 'Momentum' ) );
-            $main.appendChild( PostCard( postsData.productivityPosts ) );
+            const $fragment = document.createDocumentFragment();
+            const $section = document.createElement( 'section' );
+            const $divContainer = document.createElement( 'div' );
+            $divContainer.classList.add( 'post-container' );
+            for ( const post in postsData.productividad ) {
+                $fragment.appendChild( PostCard( postsData.productividad[ post ] ) );
+            }
+            $divContainer.appendChild( $fragment );
+            $section.appendChild( $divContainer );
+            $main.appendChild( $section );
             $loader.style.display = 'none';
         }, 500 );
     } else if ( hash === '#/mentalidad' ) {
         setTimeout( () => {
             $main.appendChild( HeaderSection( 'Voluntad Inquebrantable' ) );
-            $main.appendChild( PostCard( postsData.mindSetPosts ) );
+            const $fragment = document.createDocumentFragment();
+            const $section = document.createElement( 'section' );
+            const $divContainer = document.createElement( 'div' );
+            $divContainer.classList.add( 'post-container' );
+            for ( const post in postsData.mentalidad ) {
+                $fragment.appendChild( PostCard( postsData.mentalidad[ post ] ) );
+            }
+            $divContainer.appendChild( $fragment );
+            $section.appendChild( $divContainer );
+            $main.appendChild( $section );
             $loader.style.display = 'none';
         }, 500 );
     } else if ( hash === '#/masculinidad' ) {
         setTimeout( () => {
             $main.appendChild( HeaderSection( 'Titanes de excelencia' ) );
-            $main.appendChild( PostCard( postsData.masculinityPosts ) );
+            const $fragment = document.createDocumentFragment();
+            const $section = document.createElement( 'section' );
+            const $divContainer = document.createElement( 'div' );
+            $divContainer.classList.add( 'post-container' );
+            for ( const post in postsData.masculinidad ) {
+                $fragment.appendChild( PostCard( postsData.masculinidad[ post ] ) );
+            }
+            $divContainer.appendChild( $fragment );
+            $section.appendChild( $divContainer );
+            $main.appendChild( $section );
+            $loader.style.display = 'none';
+        }, 500 );
+    } else if ( hash.includes( 'articulo/' ) ) {
+        setTimeout( () => {
+            const categoryPost = postsData[ `${ categoryPostUrl }` ]; // recupera la categoría de la url
+            // si no existe la categoría o el id del post en la url...
+            if ( !categoryPost || !idPostUrl ) $root.innerHTML = Error404();
+            const idPost = categoryPost[ `post${ idPostUrl }` ]; // recupera el id del post de la url 
+            // si no existe el id del post...
+            if ( !idPost ) $root.innerHTML = Error404();
+            $main.appendChild( PostContainer( idPost ) );
             $loader.style.display = 'none';
         }, 500 );
     } else { // if is other route that doesn't exist...
