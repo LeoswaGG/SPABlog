c3dd2dd5703d8c216755c4afc83ef8bfb313457d
Leoswagg <pepinillosmtzswag@gmail.com>
2023-08-25 11:46:36 -0600

    Refactoriza el código cambiandolo a una sola tarjeta en vez de 6 para que se agrege una nueva tarjeta cada que cargue un post y cambia la documentación

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,104 +1,26 @@
 /**
- * The function `PostCard` creates a section element with multiple post cards based on the data
- * provided(date, title, img).
- * @param postsData - The `postsData` parameter is an object that contains the data for each post. It
- * has the following structure:
- * @returns a dynamically created HTML section element that contains multiple post cards.
+ * The `PostCard` function creates a div element that contains a postCard based on the provided
+ * data.
+ * @param postsData - The `postsData` parameter is an object that contains the data for each post.
+ * @returns The function `PostCard` returns a dynamically created HTML div element that contains
+ * a postCard.
  */
 export function PostCard ( postsData ) {
-    const $section = document.createElement( 'section' );
-    $section.innerHTML = `
-    <div class="post-container">
-        <div class="post">
+    const $divPost = document.createElement( 'div' );
+    $divPost.classList.add( 'post' );
+    $divPost.innerHTML = `
             <div class="main-content">
                 <div class="header">
                     <span>Publicación:</span>
-                    <span>${ postsData.post1.date }</span>
+                    <span>${ postsData.date }</span>
                 </div>
-                <p class="post-title">${ postsData.post1.title }</p>
+                <p class="post-title">${ postsData.title }</p>
                 <div class="img-container">
-                    <img src="app/assets/${ postsData.post1.img }" alt="post-image" class="post-img">
+                    <img src="app/assets/${ postsData.img }" alt="post-image" class="post-img">
                 </div>
             </div>
             <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>
-        <div class="post">
-            <div class="main-content">
-                <div class="header">
-                    <span>Publicación:</span>
-                    <span>${ postsData.post2.date } </span>
-                </div>
-                <p class="post-title">${ postsData.post2.title }</p>
-                <div class="img-container">
-                    <img src="app/assets/${ postsData.post2.img }" alt="post-image" class="post-img">
-                </div>
-            </div>
-            <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>
-        <div class="post">
-            <div class="main-content">
-                <div class="header">
-                    <span>Publicación:</span>
-                    <span>${ postsData.post3.date } </span>
-                </div>
-                <p class="post-title">${ postsData.post3.title }</p>
-                <div class="img-container">
-                    <img src="app/assets/${ postsData.post3.img }" alt="post-image" class="post-img">
-                </div>
-            </div>
-            <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>
-        <div class="post">
-            <div class="main-content">
-                <div class="header">
-                    <span>Publicación:</span>
-                    <span>${ postsData.post4.date } </span>
-                </div>
-                <p class="post-title">${ postsData.post4.title }</p>
-                <div class="img-container">
-                    <img src="app/assets/${ postsData.post4.img }" alt="post-image" class="post-img">
-                </div>
-            </div>
-            <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>
-        <div class="post">
-            <div class="main-content">
-                <div class="header">
-                    <span>Publicación:</span>
-                    <span>${ postsData.post5.date } </span>
-                </div>
-                <p class="post-title">${ postsData.post5.title }</p>
-                <div class="img-container">
-                    <img src="app/assets/${ postsData.post5.img }" alt="post-image" class="post-img">
-                </div>
-            </div>
-            <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>
-        <div class="post">
-            <div class="main-content">
-                <div class="header">
-                    <span>Publicación:</span>
-                    <span>${ postsData.post6.date } </span>
-                </div>
-                <p class="post-title">${ postsData.post6.title }</p>
-                <div class="img-container">
-                    <img src="app/assets/${ postsData.post6.img }" alt="post-image" class="post-img">
-                </div>
-            </div>
-            <div class="post-footer">
-                <a href="#/">Leer más...</a>
-            </div>
-        </div>        
-    </div>`;
-    return $section;
+                <a href="#/${ postsData.category }/articulo/${ postsData.id }">Leer más...</a>
+            </div>`;
+    return $divPost;
 }
